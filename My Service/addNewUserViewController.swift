6149e32ba418169046ef7f45909e5700f824a133
Student05 <sombat@snru.ac.th>
2017-06-15 16:30:58 +0700

    15June17

---
+++ My Service/addNewUserViewController.swift
@@ -0,0 +1,36 @@
+//
+//  addNewUserViewController.swift
+//  My Service
+//
+//  Created by Student05 on 6/15/2560 BE.
+//  Copyright © 2560 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class addNewUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is AddNewuser")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
